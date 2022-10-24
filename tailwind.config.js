9233584c2b76fbcbfa5339e19d312a47fb691302
Jacob Hilker <jacob.hilker2@gmail.com>
2022-10-24 18:28:10 -0400

    added heraldic colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,21 @@ module.exports = {
           white: "#dcd3bf",
           tan: "#d0c4a9"
         },
+        heraldic: {
+          silver: "#f6f6f6",
+          gold: "#f1ba51",
+          red: "#bc2e2e",
+          blue: {
+            DEFAULT: "#0d6793",
+            bright: "#1393d1"
+          },
+          black: "#333333",
+          purple: "#8f3f6a",
+          green: "#3f933e",
+          murrey: "#a42c45",
+          sanguine: "#a52f2d",
+          tenne: "#bf7532"
+        }
       },
 
 
