08bff18e2bd9cebf9d6d673504b97beac23ff293
Jacob Hilker <jacob.hilker2@gmail.com>
2022-11-13 16:24:20 -0500

    removed javascript for toggling mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,3 @@ const toggleSidebar = () => {
   let sidebar = document.querySelector("[role=mobile-sidebar]");
   sidebar.classList.toggle("-translate-x-full");
 };
-
-const toggleMobileMenu = () => {
-  document
-    .querySelector("[role=mobile-menu-header]")
-    .classList.toggle("hidden");
-};
