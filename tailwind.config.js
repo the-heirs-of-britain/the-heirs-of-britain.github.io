1c1d171dac7fcd5c2d5d843bfe42a761773acc8b
Jacob Hilker <jacob.hilker2@gmail.com>
2022-11-25 12:56:22 -0500

    changed heraldic colors, fixed active nav link not rendering

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,26 +26,22 @@ module.exports = {
         13: "52px",
         18: "66px",
       },
-
       colors: {
         manuscript: {
           white: "#dcd3bf",
           tan: "#d0c4a9",
         },
         heraldic: {
-          silver: "#f6f6f6",
-          gold: "#f1ba51",
-          red: "#bc2e2e",
-          blue: {
-            DEFAULT: "#0d6793",
-            bright: "#1393d1",
-          },
+          silver: "#fafafa",
+          gold: "#ffe066",
+          red: "#d7374a",
+          blue: "#377cd7",
           black: "#333333",
-          purple: "#8f3f6a",
-          green: "#3f933e",
-          murrey: "#a42c45",
-          sanguine: "#a52f2d",
-          tenne: "#bf7532",
+          purple: "#522d5b",
+          green: "#26c061",
+          murrey: "#85185b",
+          sanguine: "#b63a3a",
+          tenne: "#c77f19",
         },
       },
 
